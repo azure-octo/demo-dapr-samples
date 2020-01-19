41d3b9a336b936ae4711ef51a260932342afdd1b
Aaron Crawfis <aacrawfi@microsoft.com>
2020-01-18 16:44:02 -0800

    Added deploy files

---
+++ 3.distributed-calculator/cleanup.ps1
@@ -0,0 +1,2 @@
+kubectl config use-context AKS
+kubectl delete -f .\deploy
---
+++ 3.distributed-calculator/deploy.ps1
@@ -0,0 +1,14 @@
+Write-Host "---------------SWITCHING KUBECTL CONTEXT-----------------" -ForegroundColor White -BackgroundColor Black
+kubectl config use-context AKS
+Write-Host "---------------DEPLOYING REDIS BINDING-------------------" -ForegroundColor Black -BackgroundColor Blue
+kubectl apply -f .\deploy\redis.yaml
+Write-Host "---------------DEPLOYING REACT FRONTEND------------------" -ForegroundColor Black -BackgroundColor Blue
+kubectl apply -f .\deploy\react-calculator.yaml
+Write-Host "---------------DEPLOYING GO ADDER------------------------" -ForegroundColor Black -BackgroundColor Red
+kubectl apply -f .\deploy\go-adder.yaml
+Write-Host "---------------DEPLOYING C# SUBTRACTOR-------------------" -ForegroundColor Black -BackgroundColor Green
+kubectl apply -f .\deploy\dotnet-subtractor.yaml
+Write-Host "---------------DEPLOYING PYTHON MULTIPLIER---------------" -ForegroundColor Black -BackgroundColor Yellow
+kubectl apply -f .\deploy\python-multiplier.yaml
+Write-Host "---------------DEPLOYING NODE DIVIDER--------------------" -ForegroundColor Black -BackgroundColor White
+kubectl apply -f .\deploy\node-divider.yaml
